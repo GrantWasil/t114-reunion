30e5fe26b8fd8f4276f4f1ce3810c1ef8a2b78c3
GrantWasil <dev@grantwasil.com>
2021-05-27 19:34:44 -0600

    feat: implemented 'react-eventbrite-popup-checkout

--- src/components/BuyButton/BuyButton.js
+++ src/components/BuyButton/BuyButton.js
@@ -1,24 +1,12 @@
 import React from "react";
+import EventbriteButton from 'react-eventbrite-popup-checkout';
 
 function BuyButton() {
   return (
     <>
-      <button id="eventbrite-widget-modal-trigger-157009771453" type="button" onClick={handleClick}>
-        Buy Tickets
-      </button>
-      <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
-
+      <EventbriteButton ebEventId='157009771453'>Buy Tickets</EventbriteButton>
     </>
   );
-}
-
-function handleClick() {
-    window.EBWidgets.createWidget({
-        widgetType: "checkout",
-        eventId: "157009771453",
-        modal: true,
-        modalTriggerElementId: "eventbrite-widget-modal-trigger-157009771453",
-      })
 }
 
 export default BuyButton;
